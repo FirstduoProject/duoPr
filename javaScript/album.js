4942976d86e6996a3447f8c4018e3afd2e51c33c
Med.Charfeddine <charfedchaibi@gmail.com>
2024-07-12 08:31:15 +0100

    work in prog

--- javaScript/album.js
+++ javaScript/album.js
@@ -4,98 +4,80 @@ var urlParams = new URLSearchParams(window.location.search);
 var addAlbumName = urlParams.get("album");
 
 var div = document.getElementById("container");
+var currentAlbum = albums.filter(function (el) {
+  return el.name === addAlbumName;
+});
 
-for (var i = 0; i < albums.length; i++) {
-  if (albums[i].name === addAlbumName) {
-    var album = albums[i];
-    for (let j = 0; j < album.tracks.length; j++) {
-      var track = album.tracks[j];
-      div.innerHTML += `
-          <div class="cards">
-            <h3 class= "names">${track.title}</h3>
-            <h3 class= "names">${track.artist}</h3>
-            <audio controls src="${track.path}" class="musicPlayer"></audio>
-            <div class="btns"><button id = "next" onclick="NextSongs()"><i class="fa-solid fa-backward-step"></i></button>
-            <button id = "prev"><i class="fa-solid fa-forward-step"></i></button></div>
-            <img src="${track.image}">
-            </div>`}
-    break;
+var allTracks = currentAlbum[0].tracks;
+var track = currentAlbum[0].tracks[0];
+
+var container = document.getElementById("container");
+var tracksHtml = allTracks
+.map(function (song, index) {
+  return `
+      <div class="cards">
+          <h3 class="names">${song.title}</h3>
+          <h3 class="names">${song.artist}</h3>
+          <div class="btns">
+              <button class="playNowBtn" data-index="${index}"><i class="fa-solid fa-forward-step"></i></button>
+          </div>
+      </div>
+  `;
+})
+.join("");
+
+
+container.innerHTML = tracksHtml;
+
+container.addEventListener("click", function (event) {
+  if (event.target.closest(".playNowBtn")) {
+    var index = event.target.closest(".playNowBtn").dataset.index;
+    playSong(index);
   }
-} 
+});
 
+div.innerHTML += `
+      <div class="cards">
+        <h3 class="names" id="trackTitle" >${track.title}</h3>
+        <h3 class="names" id="trackArtist" >${track.artist}</h3>
+        <audio controls src="${track.path}" class="musicPlayer" id="player"></audio>
+        <div class="btns"><button id="prev"><i class="fa-solid fa-backward-step"></i></button>
+        <button id="next"><i class="fa-solid fa-forward-step"></i></button></div>
+        <img src="${track.image}">
+        </div>`;
 
+var nextBtn = document.getElementById("next");
+var prevBtn = document.getElementById("prev");
+var trackTitle = document.getElementById("trackTitle");
+var trackArtist = document.getElementById("trackArtist");
+var audioPlayer = document.getElementById("player");
+let currentIndex = 0;
 
-// div.innerHTML += `<nav>
-// <a href="musicPlayer.html" ><i class="fa-brands fa-spotify"></i></a>
-// <a href="#second"><i class="fas fa-search"></i></a>
-// <a href="#third"><i class="fa-regular fa-heart"></i></a>
-// // <a href="#fourth"><i class="far fa-address-card"></i></a> //
-// </nav> `;
-
-
-
-
-function each(array, func) {
-  for (var i = 0; i < array.length; i++) {
-    func(array[i]);
-  }
+function playSong(index) {
+  var track = allTracks[index];
+  console.log(track.path);
+  audioPlayer.src = track.path;
+  trackTitle.innerHTML = track.title;
+  trackArtist.innerHTML = track.artist;
+  audioPlayer.play();
 }
 
-function map(people, f) {
-  var arr = [];
-  each(people, function (el, i) {
-    console.log(i);
-    arr.push(f(el, i));
-  });
-  return arr;
-}
-
-
-function filter(array,predicate) {
-  var array2 = []
-
-  each(array, function(element,i) {
-      if(predicate(element,i)) {
-          array2.push(element)
-      }
-  })
-   
-  return array2
-}
-   
-
-// function searchFor() {
-//   var newWord = document.getElementById("search").value.trim().toLowerCase(); 
-
-//   var filteredTracks = filter(track, function(el) {
-//     return newWord === el.title.toLowerCase() || newWord === el.artist.toLowerCase()
-//   });
-
-//   div.style.display = "none";
-
-//   each(filteredTracks,function(track) {
-//     div.innerHTML += `
-//       <div class="cards">
-//         <h3 class="names">${track.title}</h3>
-//         <h3 class="names">${track.artist}</h3>
-//         <audio controls src="${track.path}" class="musicPlayer"></audio>
-//         <div class="btns">
-//           <button class="next"><i class="fa-solid fa-backward-step"></i></button>
-//           <button class="prev"><i class="fa-solid fa-forward-step"></i></button>
-//         </div>
-//         <img src="${track.image}">
-//       </div>`;
-//   });
-//  }
-
-
-
-  function addToFav(){
-    var fav = []
-    fav.push(album.tracks)
-    return fav
+nextBtn.addEventListener("click", function (event) {
+  if (currentIndex < allTracks.length - 1) {
+      currentIndex = currentIndex + 1;
+  } else {
+      currentIndex = 0;
   }
+  playSong(currentIndex);
+});
 
 
-document.getElementById("heart").onclick = function(){addToFav()};
-document.getElementById("btn-search").onclick = function(){searchFor()};
+prevBtn.addEventListener("click", function (event) {
+  if (currentIndex > 0) {
+      currentIndex = currentIndex - 1;
+  } else {
+      currentIndex = allTracks.length - 1;
+  }
+  playSong(currentIndex);
+});
+
