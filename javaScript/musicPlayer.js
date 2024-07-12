8e91ba8c4c293513163815b2d5e8c60afc3d6629
Khalfa Aymen <127998362+khalfaAymen@users.noreply.github.com>
2024-07-12 09:17:27 +0100

    new updates added

--- javaScript/musicPlayer.js
+++ javaScript/musicPlayer.js
@@ -1,11 +1,29 @@
 import { albums } from "./albumsVar.js";
 
+var urlParams = new URLSearchParams(window.location.search);
+var searchQuery = urlParams.get("q");
 var mydiv = document.querySelector(".album");
 
-albums.map(function (el) {
-  mydiv.innerHTML += `<div class='card'>
+if (!searchQuery) {
+  console.log(searchQuery);
+  albums.map(function (el) {
+    mydiv.innerHTML += `<div class='card'>
     <h1>${el.name}</h1>
     <h2>${el.artist}</h2>
     <a href=/html/album.html?album=${el.name}><img src=${el.image} alt=""></a>
     </div>`;
-});
+  });
+} else {
+  albums.map(function (el) {
+    if (
+      el.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      el.artist.toLowerCase().includes(searchQuery.toLowerCase())
+    ) {
+      mydiv.innerHTML += `<div class='card'>
+    <h1>${el.name}</h1>
+    <h2>${el.artist}</h2>
+    <a href=/html/album.html?album=${el.name}><img src=${el.image} alt=""></a>
+    </div>`;
+    }
+  });
+}
